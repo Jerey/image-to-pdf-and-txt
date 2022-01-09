41e17f05d3addac0da8ea15317259201603ddb00
Jerey <antonjerey@gmail.com>
2022-01-09 12:02:24 +0100

    update test.

--- tests/picture_rotator_test.py
+++ tests/picture_rotator_test.py
@@ -4,7 +4,8 @@ from imagetopdfandtxt import picture_rotator
 class picture_rotator_test(unittest.TestCase):
 
     def test_get_new_name(self):
-        self.assertEqual(picture_rotator.get_new_name("/some/path/my_image.jpg", "not_rotated"), "ASDF")
+        filename = "my_image"
+        self.assertEqual(picture_rotator.get_new_name(f"/some/path/{filename}.jpg", "not_rotated"), f"{filename}_not_rotated.jpg")
 
 if __name__ == "__main__":
     unittest.main()
